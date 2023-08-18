8967f8ea540affd0134fbca03afd21b5cc0590af
GabrielBorges2000 <gabriel.vscode@gmail.com>
2023-08-18 12:23:47 -0300

    create nextPWA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import 'prismjs/components/prism-sql';
 import 'prismjs/themes/prism-dark.css';
 
 import { useCompletion } from 'ai/react'
-import result from 'postcss/lib/result';
+import Head from 'next/head';
 
 export default function Home() {
   const [schema, setSchema] = useState('');
@@ -23,7 +23,40 @@ export default function Home() {
   })
 
   return (
+    
     <div className="max-w-[780px] mx-auto px-4 pt-12 pb-4">
+      <Head>
+        <meta name="application-name" content="AskSQL" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="AskSQL" />
+        <meta name="description" content="Gerador de SQL" />
+        <meta name="format-detection" content="telephone=+5511986237504" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#22bf08" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#07061D" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://yourdomain.com" />
+        <meta name="twitter:title" content="AskSQL" />
+        <meta name="twitter:description" content="Gerador de SQL" />
+        <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
+        <meta name="twitter:creator" content="@GabrielBorges2000" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="AskSQL" />
+        <meta property="og:description" content="Gerador de SQL" />
+        <meta property="og:site_name" content="AskSQL" />
+        <meta property="og:url" content="https://asksql-gabrielborges2000.vercel.app/" />
+        <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
+      </Head>
 
       <header className="flex items-center justify-between">
         <Logo />
